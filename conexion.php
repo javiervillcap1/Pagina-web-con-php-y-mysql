09c61d9eb065910d5df02ab57a557b760a32319a
edson <javiervillcap@gmail.com>
2022-05-16 07:24:24 -0400

    principe

---
+++ conexion.php
@@ -0,0 +1,9 @@
+
+<?php
+	$servidor = "localhost";
+	$usuario = "root";
+	$clave = "";
+	$dbname = "cancionero";
+	
+	//Criar a conexao
+	$conn = mysqli_connect($servidor, $usuario, $clave, $dbname);
